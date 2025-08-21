7cc872d55c5570dc9b798bac5015b7ab4f905423
deric10 <alfredderick10@gmail.com>
2025-08-22 00:56:18 +0300

    Feat adding required packages for the application

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold text-amber-700">
+    Hello world!
+    <button class="btn btn-primary">
+      <Icon name="tabler:map" size="23" /> Hellow deric
+    </button>
+  </h1>
 </template>
